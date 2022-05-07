1bfb5f63004ce8bcecaf1a417adce9118733e9c7
Tamás Fábián <fabian.tamas@kincsinfo.hu>
2022-05-07 23:48:15 +0200

    frontend, backend init

---
+++ api/src/index.ts
@@ -0,0 +1,16 @@
+import "module-alias/register";
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("test");
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
